0765303425ad6bd3335f72880522902ccf315b4d
Blaine Mooers <bmooers1@gmail.com>
2024-08-13 11:21:28 -0500

    Update mcopy.sh

--- mcopy.sh
+++ mcopy.sh
@@ -2,8 +2,8 @@ echo "Bash script to transfer tar files without entering the password after each
 echo "Requires the sshpass program. Available from Macports and probably elsewhere like Home Brew."
 echo "The transfer of eight files at a time is the limit on my 2018 MacBook Pro. Four or six files may be ideal."
 echo "The canary.wav file is a sound file. Any sound file will do. It gives an audible indication that the transfer is finished."
-echo "afplay is a Unix program. It might be in Mac OS X."
-echo "The `say` program on Mac OS will convert text to speech."
+echo "afplay is a Unix program that plays an audio file. It might run on Mac OS X."
+echo "The `say` program on Mac OS will convert text to speech. The espeak program is the Linux analog."
 echo "The audible alert is very helpful and easy to add to other scripts."
 for file in 1712.tar 1716.tar 1713.tar 1714.tar 1733.tar ; do
     echo "The transfer of ${file} has started."
